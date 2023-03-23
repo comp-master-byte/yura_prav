c5375ae6f82f8442c85364eb143b839c459729ce
comp-master-byte <demoniac781@gmail.com>
2023-03-24 00:14:57 +0300

    Очистка при удалении и поменял иконку

--- src/components/Account/AddLawyerItemButton/AddLawyerItemButton.js
+++ src/components/Account/AddLawyerItemButton/AddLawyerItemButton.js
@@ -1,40 +1,42 @@
 import UIOutlinedInputNotRegister from "../../../UI/UIOutlinedInput/UIOutlinedInputNotRegister";
 import styles from "./AddLawyerItemButton.module.scss";
 import { useAddLawyerItemButton } from "./hooks/useAddLawyerItemButton";
-
+import { UilTrashAlt } from "@iconscout/react-unicons";
 const AddLawyerItemButton = () => {
-    const {
-        isEdit, 
-        toggleEdition, 
-        newAnswerValueHandler,
-        newAnswerValue,
-        onSubmit,
-        answerValueError
-    } = useAddLawyerItemButton();
-    return (
-        <div className={styles.wrapper}>
-            {!isEdit && <div onClick={() => toggleEdition()} className={styles.addLawyerBtn}>
-                <i className="uil uil-plus"></i>
-            </div>}
-            {isEdit && 
-                <div className={styles.editWrapper}>
-                    <UIOutlinedInputNotRegister 
-                        placeholder="Добавить ваш ответ..."
-                        name="new_answer"
-                        value={newAnswerValue}
-                        onChange={newAnswerValueHandler}
-                        error={answerValueError}
-                    />
-                    <div onClick={onSubmit} className={styles.editWrapperSendBtn}>
-                        <i className="uil uil-message"></i>
-                    </div>
-                    <div onClick={toggleEdition} className={styles.cancelWrapperSendBtn}>
-                        <i className="uil uil-cancel"></i>
-                    </div>
-                </div>
-            }
+  const {
+    isEdit,
+    toggleEdition,
+    newAnswerValueHandler,
+    newAnswerValue,
+    onSubmit,
+    answerValueError,
+  } = useAddLawyerItemButton();
+  return (
+    <div className={styles.wrapper}>
+      {!isEdit && (
+        <div onClick={() => toggleEdition()} className={styles.addLawyerBtn}>
+          <i className="uil uil-plus"></i>
         </div>
-    )
-}
+      )}
+      {isEdit && (
+        <div className={styles.editWrapper}>
+          <UIOutlinedInputNotRegister
+            placeholder="Добавить ваш ответ..."
+            name="new_answer"
+            value={newAnswerValue}
+            onChange={newAnswerValueHandler}
+            error={answerValueError}
+          />
+          <div onClick={onSubmit} className={styles.editWrapperSendBtn}>
+            <i className="uil uil-message"></i>
+          </div>
+          <div onClick={toggleEdition} className={styles.cancelWrapperSendBtn}>
+            <UilTrashAlt />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default AddLawyerItemButton;
--- src/components/Account/AddLawyerItemButton/hooks/useAddLawyerItemButton.js
+++ src/components/Account/AddLawyerItemButton/hooks/useAddLawyerItemButton.js
@@ -1,45 +1,46 @@
 import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import LawyerService from "../../../../services/LawyerService";
-import {Context} from "../../../../index"
+import { Context } from "../../../../index";
 
 export const useAddLawyerItemButton = () => {
-    const {id} = useParams();
+  const { id } = useParams();
 
-    const {lawyer} = useContext(Context);
-    const [newAnswerValue, setNewAnswerValue] = useState('');
-    const [answerValueError, setAnswerValueError] = useState('');
-    const [isEdit, setIsEdit] = useState(false);
+  const { lawyer } = useContext(Context);
+  const [newAnswerValue, setNewAnswerValue] = useState("");
+  const [answerValueError, setAnswerValueError] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
 
-    const newAnswerValueHandler = (e) => {
-        const message = e.target.value;
-        setNewAnswerValue(message);
-        if(message) {
-            setAnswerValueError('')
-        } else {
-            setAnswerValueError('Поле не может быть пустым!');
-        }
-    };
-
-    const toggleEdition = () => setIsEdit(prev => !prev);
-
-    const onSubmit = () => {
-        if(!newAnswerValue) {
-            setAnswerValueError('Поле не может быть пустым!');
-        } else {
-            LawyerService.addNewLawyerAnswer(id, {new_answer: newAnswerValue});
-            const modifiedData = [newAnswerValue, '0000'];
-            lawyer.setLawyerHelpAnswers(modifiedData);
-            setIsEdit(false);
-        }
+  const newAnswerValueHandler = (e) => {
+    const message = e.target.value;
+    setNewAnswerValue(message);
+    if (message) {
+      setAnswerValueError("");
+    } else {
+      setAnswerValueError("Поле не может быть пустым!");
     }
+  };
 
-    return {
-        isEdit,
-        toggleEdition,
-        onSubmit,
-        newAnswerValueHandler,
-        newAnswerValue,
-        answerValueError
+  const toggleEdition = () => setIsEdit((prev) => !prev);
+
+  const onSubmit = () => {
+    if (!newAnswerValue) {
+      setAnswerValueError("Поле не может быть пустым!");
+    } else {
+      LawyerService.addNewLawyerAnswer(id, { new_answer: newAnswerValue });
+      const modifiedData = [newAnswerValue, "0000"];
+      lawyer.setLawyerHelpAnswers(modifiedData);
+      setIsEdit(false);
+      setNewAnswerValue("");
     }
-}
+  };
+
+  return {
+    isEdit,
+    toggleEdition,
+    onSubmit,
+    newAnswerValueHandler,
+    newAnswerValue,
+    answerValueError,
+  };
+};
